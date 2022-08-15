947f5a353c6cce7c9dbe2bfac9d98c02f8eadfee
somers-all-the-time <nsomers97@gmail.com>
2022-08-15 07:37:32 -0500

    Parameterize polybar launch.sh and clean up config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,22 +6,24 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# This sets up each new instance of polybar with a different MONITOR environment variable
-# for each execution
-# Might be useful in the future to parameterize monitor setting in polybar config
-# for m in $(polybar --list-monitors | cut -d":" -f1); do -> outputs 'eDP-1' while just on laptop
-#     if (monitor is internal laptop screen i.e. HiDPI
-#     if (monitor is landscape orientation):
-#       MONITOR=$m polybar --reload main &
-#     else (monitor is external i.e. standard DPI, irrelvant if it is portrait)
-#       MONITOR=$m polybar --reload portrait &
-# done
+IFS=$'\n'
 
+for textoutput in $(polybar --list-monitors)
+do
+    MYMONITOR=$(echo $textoutput | cut -d":" -f1)
+    WIDTH=$(echo $textoutput | cut -d" " -f2 | cut -d"x" -f1)
+    HEIGHT=$(echo $textoutput | cut -d" " -f2 | cut -d"x" -f2 | cut -d"+" -f1)
+    DIFF=$((WIDTH-HEIGHT))
+    if [ $DIFF -gt 0 ]
+    then
+        # echo "LANDSCAPE"
+        MONITOR=$MYMONITOR polybar -q main &
+    else
+        # echo "PORTRAIT"
+        MONITOR=$MYMONITOR polybar -q portrait &
+    fi
+done
 
-# Launch bars
-polybar -q main &
-polybar -q external &
-polybar -q external-vertical &
-polybar -q external-vertical-work &
+unset IFS
 
 echo "Bars launched..."
